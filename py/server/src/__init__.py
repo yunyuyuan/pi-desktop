ef31a200af4b8e62fced7398567c5d3df68c3a46
chenq <chenqiang@exiao.tech>
2022-01-18 18:46:11 +0800

    path

--- py/server/src/__init__.py
+++ py/server/src/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from json import load
-from os.path import realpath
+from pathlib import Path
 
 app = Flask(__name__)
 app.debug = True
@@ -10,6 +10,6 @@ app.config['SECRET_KEY'] = '15ASDF3q$R#QHae574#RFQjw543qaGQ%#$HWB#'
 def start():
     from . import http_
     from . import socket_
-    with open(realpath('../../../config.json')) as fp:
+    with open(Path('../../../config.json').resolve()) as fp:
         json = load(fp)
         app.run(port=json['port'])
