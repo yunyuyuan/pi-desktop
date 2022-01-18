b932f3d67d7eb70f0eddffb7777e2a8c9090b01f
chenq <chenqiang@exiao.tech>
2022-01-18 18:53:30 +0800

    path

--- py/server/src/__init__.py
+++ py/server/src/__init__.py
@@ -10,6 +10,6 @@ app.config['SECRET_KEY'] = '15ASDF3q$R#QHae574#RFQjw543qaGQ%#$HWB#'
 def start():
     from . import http_
     from . import socket_
-    with open(Path('../../../config.json').resolve()) as fp:
+    with open(Path(__file__, '../config.json').resolve()) as fp:
         json = load(fp)
         app.run(port=json['port'])
